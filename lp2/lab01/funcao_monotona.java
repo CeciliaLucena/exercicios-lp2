00880d6f0d57f43daa317dc56bca3e3edcbb42f8
123110814 MARIA CECILIA GOUVEIA LUCENA <maria.cecilia.gouveia.lucena@lcc3-097.lcc.ufcg.edu.br>
2024-02-06 09:47:35 -0300

    codigo em processo

---
+++ lp2/lab01/funcao_monotona.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class funcao_monotona {
+	public static void maind(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = nextInt();
+		int b = nextInt();
