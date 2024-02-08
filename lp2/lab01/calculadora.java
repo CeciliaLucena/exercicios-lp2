f19e677897849d8f5eb765c5de59e1617b44d14f
123110814 MARIA CECILIA GOUVEIA LUCENA <maria.cecilia.gouveia.lucena@lcc3-002.lcc.ufcg.edu.br>
2024-02-08 10:42:08 -0300

    lab 08/02

--- lp2/lab01/calculadora.java
+++ lp2/lab01/calculadora.java
@@ -2,36 +2,32 @@ import java.util.Scanner;
 public class calculadora {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String operacao = sc.next();
-		if (operacao == "+") {
-			float a = sc.nextFloat();
-                        float b = sc.nextFloat();
-			float resultado = (a + b);
-			System.out.println("RESULTADO: " + resultado);
-		}
-		if (operacao == "-") {
-			float a = sc.nextFloat();
-                        float b = sc.nextFloat();
-			float resultado = (a - b);
-			System.out.println("RESULTADO: " + resultado);
-		}
-		if (operacao == "*") {
-			float a = sc.nextFloat();
-                        float b = sc.nextFloat();
-			float resultado = (a * b);
-			System.out.println("RESULTADO: " + resultado);
-		}
-		if (operacao == "/") {
-			float a = sc.nextFloat();
-                        float b = sc.nextFloat();
-			if (b == 0) {
-				System.out.println("ERRO");
-			} else {
-				float resultado = (a / b);
+		String op = sc.nextLine();
+		if ("+".equals(op) || "-".equals(op) || "*".equals(op) || "/".equals(op)) {
+			float a = Float.parseFloat(sc.nextLine());
+			float b = Float.parseFloat(sc.nextLine());
+			if ("+".equals(op)) {
+				float resultado = (a + b);
 				System.out.println("RESULTADO: " + resultado);
 			}
+			if ("-".equals(op)) {
+				float resultado = (a - b);
+				System.out.println("RESULTADO: " + resultado);
+			}
+			if ("*".equals(op)) {
+				float resultado = (a * b);
+				System.out.println("RESULTADO: " + resultado);
+			}
+			if ("/".equals(op)) {
+				if (b == 0) {
+					System.out.println("ERRO");
+				} else {
+					float resultado = (a / b);
+					System.out.println("RESULTADO: " + resultado);
+				}
+			}
+		} else {
+			System.out.println("ENTRADA INVALIDA");
 		}
-		
 	}
 }
-
